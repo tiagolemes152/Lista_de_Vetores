5ab10cf5ca2805da834a063757ecb740cb12e3e0
tiagolemes152 <153042287+tiagolemes152@users.noreply.github.com>
2025-09-21 23:31:06 -0300

    atualiza projeto

--- Lista_de_Vetores/a_03.cs
+++ Lista_de_Vetores/a_03.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class a_03
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite o tamanho do vetor: ");
+        int n = int.Parse(Console.ReadLine());
+        double[] vetor = new double[n];
+        Random rand = new Random();
+        for (int i = 0; i < n; i++)
+            vetor[i] = rand.NextDouble() * 100;
+
+        Console.WriteLine("Vetor: " + string.Join(", ", vetor));
+        double menor = MenorElemento(vetor);
+        Console.WriteLine("Menor elemento: " + menor);
+    }
+
+    static double MenorElemento(double[] v)
+    {
+        double menor = v[0];
+        foreach (double num in v)
+            if (num < menor) menor = num;
+        return menor;
+    }
+}
