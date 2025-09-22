5b486e5791d35f137c138ce7795c0eebd574ec10
tiagolemes152 <153042287+tiagolemes152@users.noreply.github.com>
2025-09-21 23:09:49 -0300

    atualiza projeto

--- Lista_de_Vetores/a_02.cs
+++ Lista_de_Vetores/a_02.cs
@@ -1,5 +1,4 @@
-//Escreva um programa que leia ou gere um vetor de N elementosinteiros. A seguir, crie uma função que receba
-//esse vetor e conte quantos valores impares existem no vetor. Retorne a quantidade de impares.
+
 using System;
 class Program
 {
@@ -12,8 +11,7 @@ class Program
         Random rand = new Random();
         for (int i = 0; i < n; i++)
         {
-            vetor[i] = rand.Next(1, 100); // Gera números aleatórios entre 1 e 99
-        }
+            vetor[i] = rand.Next(1, 100); 
 
         Console.WriteLine("Vetor gerado: " + string.Join(", ", vetor));
 
