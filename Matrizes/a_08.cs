548270d1eff1a35550db4011b5bcb229984125a2
Tiago Lemes da Cruz <153042287+tiagolemes152@users.noreply.github.com>
2025-09-22 21:19:14 -0300

    Update a_08.cs
    atualização do código

--- Matrizes/a_08.cs
+++ Matrizes/a_08.cs
@@ -1,17 +1,24 @@
 using System;
-using System.Collections.Generic;
 
 class Program {
     static void Main() {
         int n = int.Parse(Console.ReadLine());
-        var set = new HashSet<string>();
+        string[,] matriz = new string[n, 2];
         int resposta = 0;
+
         for (int i = 0; i < n; i++) {
             var p = Console.ReadLine().Split();
-            string chave = p[0] + "," + p[1];
-            if (set.Contains(chave)) resposta = 1;
-            set.Add(chave);
+            matriz[i, 0] = p[0];
+            matriz[i, 1] = p[1];
+
+            for (int j = 0; j < i; j++) {
+                if (matriz[j, 0] == matriz[i, 0] && matriz[j, 1] == matriz[i, 1]) {
+                    resposta = 1;
+                    break;
+                }
+            }
         }
+
         Console.WriteLine(resposta);
     }
 }
